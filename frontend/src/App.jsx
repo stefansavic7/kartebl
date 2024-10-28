8f64e2b37de6f29afa09db793c6195f43184d782
stefansavic7 <savicstefan20000@gmail.com>
2024-10-28 12:05:36 +0100

    Implemented routing through app (not final version), added basic page components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import NavBar from "./components/NavBar";
-import SendEmail from "./components/SendEmail";
-import CallUs from "./components/CallUs";
+// import NavBar from "./components/NavBar";
+// import SendEmail from "./components/SendEmail";
+// import CallUs from "./components/CallUs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Tickets from "./pages/Tickets";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
-    <>
-      <NavBar></NavBar>
-      <SendEmail></SendEmail>
-      <CallUs></CallUs>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home/>} />
+          <Route path="kontakt" element={<Contact />} />
+          <Route path="karte" element={<Tickets />} />
+          <Route path="info" element={<About />} />
+          <Route path="*" element={<div>ERROR 404</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import NavBar from './NavBar';
+import Footer from './Footer';
+
+
+
+const Layout = () => {
+  return (
+   <>
+    <NavBar />
+    <Outlet />
+    <Footer />
+   </>
+  )
+}
+
+export default Layout
