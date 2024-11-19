1721e71c0c651aaea969ea66c4dad4e4cf5f348c
igortodic02 <igor.todic@student.etf.unibl.org>
2024-11-20 00:45:30 +0100

    Komit 5 vrsta input polja.

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function Input({ fieldType, size = '20rem' }) {
+  const renderTextField = () => {
+    const dynamicStyles = {
+      width: size,
+      '& .MuiOutlinedInput-root': {
+        borderRadius: '0.5rem',
+        border: '1px solid #d1d5db',
+        '& fieldset': {
+          borderColor: 'gray',
+        },
+        '&:hover fieldset': {
+          borderColor: '#ec4899',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: '#ec4899',
+        }
+      },
+      '& .MuiInputLabel-root': {
+        color: 'gray',
+        transition: 'all 0.3s ease',
+        '&.Mui-focused': {
+          color: '#ec4899',
+        },
+      },
+      '& .MuiInputLabel-shrink': {
+        transform: 'translate(11px, -9px) scale(0.75)',
+        backgroundColor: '#ffffff',
+        padding: '0 7px',
+        transition: 'all 0.3s ease',
+      },
+      '& .MuiInputBase-input': {
+        color: 'black',
+        '&::placeholder': {
+          color: '#6b7280',
+        },
+      },
+    };
+
+    switch (fieldType) {
+        case 'outlined-required':
+            return (
+              <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                defaultValue="Hello World"
+                sx={dynamicStyles}
+              />
+            );
+        case 'outlined-disabled':
+            return (
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="Disabled"
+                defaultValue="Hello World"
+                sx={dynamicStyles}
+              />
+            );
+        case 'password':
+            return (
+            <TextField
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                sx={dynamicStyles}
+            />
+            );
+        case 'number':
+            return (
+            <TextField
+                id="outlined-number"
+                label="Number"
+                type="number"
+                sx={dynamicStyles}
+            />
+            );
+        case 'helperText':
+            return (
+            <TextField
+                id="outlined-helperText"
+                label="Helper text"
+                defaultValue="Default Value"
+                helperText="Some important text"
+                sx={dynamicStyles}
+            />
+            );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{ '& .MuiTextField-root': { m: 1 } }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>{renderTextField()}</div>
+    </Box>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import Input from "../components/Input";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+    <Input fieldType="password"  ></Input>
+    <Input fieldType='outlined-required' size='30rem'  ></Input>
+    <Input fieldType='outlined-disabled'  ></Input>
+    <Input fieldType='number'  ></Input>
+    <Input fieldType='helperText'  ></Input>
+    
+
+    </div>
   )
 }
 
