0a2c1e8e5d97512ac187025a53796216d514c066
igortodic02 <igor.todic@student.etf.unibl.org>
2024-11-30 19:55:31 +0100

    Footer

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -64,14 +64,14 @@ const Profile = () => {
             </div>
           ))}
           <div>
-              <button className="hover:opacity-40 transition flex items-center gap-1 pt-3 mt-24">
+              <button className="hover:opacity-40 transition flex items-center gap-1 pt-11 mt-24">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" /></svg>
                   Odjava
               </button>
           </div>
         </div>
       </div>
-      <div className="bg-zinc-100 w-[68rem] h-[35rem] rounded-2xl ml-5 mt-5 mb-5">{divContents[activeIndex]}</div>
+      <div className="bg-zinc-100 w-[68rem] h-[37rem] rounded-2xl ml-5 mt-5 mb-5">{divContents[activeIndex]}</div>
     </div>
   );
 };
