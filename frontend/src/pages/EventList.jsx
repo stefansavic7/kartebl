ae9d131ecd219506671ad44db4659ce04e8aae24
Ognjen <milekic.ognjen@gmail.com>
2025-01-18 11:17:38 +0100

    komit EventList

--- frontend/src/pages/EventList.jsx
+++ frontend/src/pages/EventList.jsx
@@ -1,66 +1,39 @@
 {/* Ova stranica prikazuje listu dogadjaja Administratoru. Administrator moze pregledati, odobriti ili odbaciti dogadjaj */}
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import axios from "axios";
 
 
 
 export const EventList =()=>{
-    const [eventId,setEventId]=useState("");
-    const [eventName,setEventName]=useState("");
-    const [erorMessage,setErorMessage]=useState("");
 
-    const fettchEventName=()=>{
-        if(eventId===""){
-            setErorMessage("morate unijeti id!");
-            setEventName("");
-            return;
+    const[events,setEvents]=useState([]);
+    //const[loading,setLoading]=useState(true);  //indikator ucitavanja
+    const[error,setError]=useState(null);
+
+    const fetchEvents= async ()=>{
+        console.log("stigao saam!")
+        try{
+            const response=await axios.get(`http://localhost:9000/dogadjaji`);
+            setEvents(response.data);
+            //setLoading(false);
+        }catch(err){
+            console.error("Greska prilikom ucitavanja dogadjaja:",err);
+            setError("Ne možemo ucitati dogadjaje. Probajte kasnije.");
+            //setLoading(false);
         }
+    };
 
-        //slanje get zahtjeva na server
-
-        axios
-        .get(`http://localhost:9000/dogadjaji/${eventId}`)
-        .then((response)=>{
-            setEventName(response.data.naziv);
-            setErorMessage("")
-        }).catch((error) => {
-            setErorMessage("Došlo je do greške. Pokušajte ponovo.");
-            setEventName(""); // Očisti naziv ako dođe do greške
-        });
-
-
-    }
-
+    useEffect(()=>{
+        fetchEvents();
+    },[]);
 
     return(
         <div>
-            <p>Dogadjaji:</p>
-            <div>
-                <label>Unesi ID dogadjaja</label>
-                <input type="text" 
-                id="eventId"
-                value={eventId}
-                className="border border-gray-300 p-2 rounded w-full"
-                onChange={(e)=>setEventId(e.target.value)}
-                placeholder="unesi id"
-                />
-            </div>
-            <button
-            onClick={fettchEventName}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >Pretrazi</button>
-
-            <div>
-                {erorMessage?(
-                    <p>{erorMessage}</p>
-                ):(
-                    eventName && (
-                        <p>Naziv dogadjaja: {eventName}</p>
-                    )
-                )}
-            </div>
-
+            Lista dogadjaja:
         </div>
     );
+    
 };
+
+//ovde sam stigao. ucitao sam dogadjaje iz baze, sad ih trebam prikazati.
