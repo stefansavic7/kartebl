5b485f0ab9516d220bf8b509837c3dd88b2b52fc
damsta02 <damjan.milekic@student.etf.unibl.org>
2025-01-21 12:47:25 +0100

    dodavanje funkcionalnosti: update dogadjaj

--- backend/KarteBL_BackendAplikacija/src/main/java/org/unibl/etf/kartebl_backendaplikacija/services/impl/DogadjajServiceImpl.java
+++ backend/KarteBL_BackendAplikacija/src/main/java/org/unibl/etf/kartebl_backendaplikacija/services/impl/DogadjajServiceImpl.java
@@ -3,8 +3,11 @@ package org.unibl.etf.kartebl_backendaplikacija.services.impl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 import org.unibl.etf.kartebl_backendaplikacija.base.CrudJpaService;
+import org.unibl.etf.kartebl_backendaplikacija.exceptions.NotFoundException;
 import org.unibl.etf.kartebl_backendaplikacija.models.dto.DogadjajDto;
 import org.unibl.etf.kartebl_backendaplikacija.models.entities.AdministratorEntity;
 import org.unibl.etf.kartebl_backendaplikacija.models.entities.DogadjajEntity;
@@ -56,6 +59,41 @@ public class DogadjajServiceImpl extends CrudJpaService<DogadjajEntity, Integer>
             e.printStackTrace();
             return null;
         }
+    }
+    public DogadjajDto update( Integer id,  DogadjajRequest podaci,  MultipartFile slika)
+    {
+        if (!dogadjajRepository.existsById(id))
+            throw new NotFoundException();
+
+        DogadjajEntity dogadjaj = new DogadjajEntity();
+        dogadjaj.setId(id);
+        dogadjaj.setDatum(podaci.getDatum());
+        dogadjaj.setNaziv(podaci.getNaziv());
+        dogadjaj.setVrijeme(podaci.getVrijeme());
+        dogadjaj.setLokacija(podaci.getLokacija());
+        dogadjaj.setOpis(podaci.getOpis());
+        AdministratorEntity administratorEntity = new AdministratorEntity();
+        administratorEntity.setId(podaci.getAdministratorId());
+        dogadjaj.setAdministrator(administratorEntity);
+        OrganizatorEntity organizatorEntity = new OrganizatorEntity();
+        organizatorEntity.setId(podaci.getOrganizatorId());
+        dogadjaj.setOrganizator(organizatorEntity);
+        dogadjaj.setOdobren(false);
+        dogadjaj.setTipSlike(slika.getContentType());
+        try {
+            byte[] slikaFile = slika.getBytes();
+            dogadjaj.setSlika(slikaFile);
+            dogadjaj=dogadjajRepository.saveAndFlush(dogadjaj);
+            DogadjajDto response = new DogadjajDto();
+            modelMapper.map(dogadjaj, response);
+            return response;
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
 
