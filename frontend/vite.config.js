ad69765a677ff8ba5b43f29b961220cefd6c0474
igortodic02 <igor.todic@student.etf.unibl.org>
2025-03-15 18:13:33 +0100

    Kreiranje stranica za dogadjaje

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -9,4 +9,32 @@ export default defineConfig({
       '/api': 'http://localhost:8080'
     }
   }
+})*/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import history from 'connect-history-api-fallback'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    {
+      name: 'html-history-api-fallback',
+      configureServer(server) {
+        server.middlewares.use(
+          history({
+            // disableDotRule allows paths containing dots (.) to be handled correctly
+            disableDotRule: true,
+            htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'],
+          })
+        )
+      }
+    }
+  ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  }
 })
+
