64a35b9e580d20c21a480509c1f8e9a8d833c25d
igortodic02 <igor.todic@student.etf.unibl.org>
2025-03-15 18:19:04 +0100

    Kreiranje stranica za dogadjaje

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,3 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  }
-})*/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import history from 'connect-history-api-fallback'
